267478b58228609018aac4cdb0dfe9874cfa9222
akasajal <me.sajal08@gmail.com>
2026-09-18 03:06:18 +0530

    feat(taxonomy): map Egyptian cat and feline classes to Tabby Cat (Egyptian Mau) with Felis catus binomial name

--- app/src/main/java/com/animdex/app/data/AnimalDictionary.kt
+++ app/src/main/java/com/animdex/app/data/AnimalDictionary.kt
@@ -80,16 +80,111 @@ object AnimalDictionary {
         )
     }
 
+    private val GENERAL_TAXONOMY = mapOf(
+        "egyptian cat" to Pair("Tabby Cat (Egyptian Mau)", "Felis catus"),
+        "tabby" to Pair("Tabby Cat", "Felis catus"),
+        "tabby cat" to Pair("Tabby Cat", "Felis catus"),
+        "tiger cat" to Pair("Tabby Cat (Mackerel)", "Felis catus"),
+        "persian cat" to Pair("Persian Cat", "Felis catus"),
+        "siamese cat" to Pair("Siamese Cat", "Felis catus"),
+        "cougar" to Pair("Cougar / Mountain Lion", "Puma concolor"),
+        "lynx" to Pair("Lynx", "Lynx lynx"),
+        "leopard" to Pair("Leopard", "Panthera pardus"),
+        "snow leopard" to Pair("Snow Leopard", "Panthera uncia"),
+        "jaguar" to Pair("Jaguar", "Panthera onca"),
+        "lion" to Pair("Lion", "Panthera leo"),
+        "tiger" to Pair("Tiger", "Panthera tigris"),
+        "cheetah" to Pair("Cheetah", "Acinonyx jubatus"),
+        "brown bear" to Pair("Brown Bear / Grizzly", "Ursus arctos"),
+        "polar bear" to Pair("Polar Bear", "Ursus maritimus"),
+        "sloth bear" to Pair("Sloth Bear", "Melursus ursinus"),
+        "giant panda" to Pair("Giant Panda", "Ailuropoda melanoleuca"),
+        "red fox" to Pair("Red Fox", "Vulpes vulpes"),
+        "grey fox" to Pair("Grey Fox", "Urocyon cinereoargenteus"),
+        "arctic fox" to Pair("Arctic Fox", "Vulpes lagopus"),
+        "kit fox" to Pair("Kit Fox", "Vulpes macrotis"),
+        "timber wolf" to Pair("Grey Wolf", "Canis lupus"),
+        "white wolf" to Pair("Arctic Wolf", "Canis lupus arctos"),
+        "coyote" to Pair("Coyote", "Canis latrans"),
+        "golden retriever" to Pair("Golden Retriever", "Canis lupus familiaris"),
+        "german shepherd" to Pair("German Shepherd", "Canis lupus familiaris"),
+        "labrador retriever" to Pair("Labrador Retriever", "Canis lupus familiaris"),
+        "beagle" to Pair("Beagle", "Canis lupus familiaris"),
+        "boxer" to Pair("Boxer", "Canis lupus familiaris"),
+        "rottweiler" to Pair("Rottweiler", "Canis lupus familiaris"),
+        "dalmatian" to Pair("Dalmatian", "Canis lupus familiaris"),
+        "collie" to Pair("Collie", "Canis lupus familiaris"),
+        "african elephant" to Pair("African Elephant", "Loxodonta africana"),
+        "indian elephant" to Pair("Asian Elephant", "Elephas maximus"),
+        "hippopotamus" to Pair("Hippopotamus", "Hippopotamus amphibius"),
+        "zebra" to Pair("Zebra", "Equus quagga"),
+        "giraffe" to Pair("Giraffe", "Giraffa camelopardalis"),
+        "bison" to Pair("American Bison", "Bison bison"),
+        "ox" to Pair("Wild Ox / Cattle", "Bos taurus"),
+        "water buffalo" to Pair("Water Buffalo", "Bubalus bubalis"),
+        "wild boar" to Pair("Wild Boar", "Sus scrofa"),
+        "warthog" to Pair("Warthog", "Phacochoerus africanus"),
+        "red deer" to Pair("Red Deer", "Cervus elaphus"),
+        "white-tailed deer" to Pair("White-Tailed Deer", "Odocoileus virginianus"),
+        "moose" to Pair("Moose / Elk", "Alces alces"),
+        "gazelle" to Pair("Gazelle", "Gazella"),
+        "impala" to Pair("Impala", "Aepyceros melampus"),
+        "llama" to Pair("Llama", "Lama glama"),
+        "alpaca" to Pair("Alpaca", "Vicugna pacos"),
+        "camel" to Pair("Camel", "Camelus"),
+        "chimpanzee" to Pair("Chimpanzee", "Pan troglodytes"),
+        "gorilla" to Pair("Western Gorilla", "Gorilla gorilla"),
+        "orangutan" to Pair("Orangutan", "Pongo"),
+        "koala" to Pair("Koala", "Phascolarctos cinereus"),
+        "kangaroo" to Pair("Red Kangaroo", "Osphranter rufus"),
+        "wallaby" to Pair("Wallaby", "Notamacropus"),
+        "platypus" to Pair("Duck-Billed Platypus", "Ornithorhynchus anatinus"),
+        "echidna" to Pair("Short-Beaked Echidna", "Tachyglossus aculeatus"),
+        "otter" to Pair("River Otter", "Lontra canadensis"),
+        "sea otter" to Pair("Sea Otter", "Enhydra lutris"),
+        "badger" to Pair("European Badger", "Meles meles"),
+        "honey badger" to Pair("Honey Badger", "Mellivora capensis"),
+        "skunk" to Pair("Striped Skunk", "Mephitis mephitis"),
+        "raccoon" to Pair("Common Raccoon", "Procyon lotor"),
+        "red panda" to Pair("Red Panda", "Ailurus fulgens"),
+        "beaver" to Pair("North American Beaver", "Castor canadensis"),
+        "porcupine" to Pair("Crested Porcupine", "Hystrix cristata"),
+        "squirrel" to Pair("Tree Squirrel", "Sciurus"),
+        "chipmunk" to Pair("Eastern Chipmunk", "Tamias striatus"),
+        "hedgehog" to Pair("European Hedgehog", "Erinaceus europaeus"),
+        "rabbit" to Pair("European Rabbit", "Oryctolagus cuniculus"),
+        "hare" to Pair("European Hare", "Lepus europaeus"),
+        "american alligator" to Pair("American Alligator", "Alligator mississippiensis"),
+        "nile crocodile" to Pair("Nile Crocodile", "Crocodylus niloticus"),
+        "green iguana" to Pair("Green Iguana", "Iguana iguana"),
+        "chameleon" to Pair("Chameleon", "Chamaeleonidae"),
+        "komodo dragon" to Pair("Komodo Dragon", "Varanus komodoensis"),
+        "leatherback turtle" to Pair("Leatherback Sea Turtle", "Dermochelys coriacea"),
+        "green sea turtle" to Pair("Green Sea Turtle", "Chelonia mydas"),
+        "great white shark" to Pair("Great White Shark", "Carcharodon carcharias"),
+        "tiger shark" to Pair("Tiger Shark", "Galeocerdo cuvier"),
+        "hammerhead shark" to Pair("Hammerhead Shark", "Sphyrna"),
+        "killer whale" to Pair("Orca / Killer Whale", "Orcinus orca"),
+        "blue whale" to Pair("Blue Whale", "Balaenoptera musculus"),
+        "humpback whale" to Pair("Humpback Whale", "Megaptera novaeangliae"),
+        "bottlenose dolphin" to Pair("Common Bottlenose Dolphin", "Tursiops truncatus")
+    )
+
     fun getInfoForGeneralModel(index: Int, rawLabel: String): AnimalInfo {
         val parsed = parseSpeciesLabel(rawLabel)
-        val clean = parsed.displayName.lowercase()
+        val clean = parsed.displayName.lowercase().trim()
+        val mapped = GENERAL_TAXONOMY[clean] ?: GENERAL_TAXONOMY.entries.firstOrNull { clean.contains(it.key) }?.value
+
+        val finalDisplayName = mapped?.first ?: parsed.displayName
+        val finalScientificName = mapped?.second ?: parsed.scientificName
+
         val group = detectGeneralGroup(index, clean)
         val isAnimal = group.isAnimal
         val funFact = getFact(clean, group)
 
         return AnimalInfo(
-            displayName = parsed.displayName,
-            scientificName = parsed.scientificName,
+            displayName = finalDisplayName,
+            scientificName = finalScientificName,
             group = group,
             isAnimal = isAnimal,
             funFact = funFact
